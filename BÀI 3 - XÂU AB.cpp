b58b479c4df58a4e45f563a86443032c066ee247
nhhoang14 <nhhoangne@gmail.com>
2025-08-03 17:27:37 +0700

    update

--- BÀI 3 - XÂU AB.cpp
+++ BÀI 3 - XÂU AB.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <string>
+
 using namespace std;
+typedef long long ll;
+
+vector<vector<ll>> C(61, vector<ll>(61, -1));
+
+ll Ckn(ll n, ll k) {
+    if (k < 0 || k > n) return 0;
+    if (C[n][k] != -1) return C[n][k];
+    if (k == 0 || k == n) return C[n][k] = 1;
+    return C[n][k] = Ckn(n - 1, k - 1) + Ckn(n - 1, k);
+}
+
+string findKthString(ll a, ll b, ll k) {
+    string result;
+    while (a > 0 && b > 0) {
+        ll cnt = Ckn(a + b - 1, a - 1);
+        if (k <= cnt) {
+            result += 'a';
+            a--;
+        } else {
+            result += 'b';
+            k -= cnt;
+            b--;
+        }
+    }
+    result += string(a, 'a');
+    result += string(b, 'b');
+    return result;
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    int a, b, k;
+    
+    ll a, b, k;
     cin >> a >> b >> k;
-    vector<char> res (a, 'a');
-    while(b--) {
-        res.push_back('b');
-    }
-    do{
-        if(k == 1){ 
-            for(auto x : res) {
-                cout << x;
-            }
-        } else if (k > 1) continue;
-        else break;
-    } while(next_permutation(res.begin(), res.end()) && k--);
+    cout << findKthString(a, b, k);
     return 0;
 }
