57a3e376fa578c9812530fe37c5261a78cd4282b
nhhoang14 <nhhoangne@gmail.com>
2025-08-03 10:29:02 +0700

    update file

--- BÀI 3 - XÂU AB.cpp
+++ BÀI 3 - XÂU AB.cpp
@@ -2,56 +2,24 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-typedef long long ll;
-
-ll Ckn(int n, int k) {
-    if (k < 0 || k > n) return 0;
-    ll res = 1;
-    for (int i = 1; i <= k; i++) {
-        res = res * (n - i + 1) / i;
-    }
-    return res;
-}
-
-char charAt(int a, int b, long long k, int idx) {
-    for (int i = 0; i <= a + b; ++i) {
-        if (a == 0) return 'b';
-        if (b == 0) return 'a';
-        long long cnt = Ckn(a + b - 1, a - 1);
-        if (k <= cnt) {
-            if (i == idx) return 'a';
-            --a;
-        } else {
-            k -= cnt;
-            if (i == idx) return 'b';
-            --b;
-        }
-    }
-    return '?'; // không xảy ra
-}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-
-    ll a, b, k;
+    int a, b, k;
     cin >> a >> b >> k;
-
-    string res = "";
-    while(a > 0 && b > 0) {
-        ll cnt = Ckn(a + b - 1, b - 1);
-        cout << "Ckn(" << a + b - 1 << ", " << b - 1 << ") = " << cnt << endl;
-        if (cnt >= k) {
-            res += 'a';
-            a--;
-        } else {
-            k -= cnt;
-            res += 'b';
-            b--;
-        }
+    vector<char> res (a, 'a');
+    while(b--) {
+        res.push_back('b');
     }
-    res += string(a, 'a') + string(b, 'b');
-    cout << res ;
+    do{
+        if(k == 1){ 
+            for(auto x : res) {
+                cout << x;
+            }
+        } else if (k > 1) continue;
+        else break;
+    } while(next_permutation(res.begin(), res.end()) && k--);
     return 0;
 }
